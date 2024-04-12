100aeda600c058ffb073ad032faa9a6c38885c03
neelpatel1111 <neelpatelnp111@gmail.com>
2024-04-12 11:27:05 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,48 @@
+projects = [
+    {
+        name: "Investment webite",
+        technologies: ["React JS", "Firebase", "Bootstrap"],
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam officiis culpa, eius pariatur repellat blanditiis mollitia cupiditate eligendi magni quasi natus suscipit sequi maxime laborum qui explicabo iste a!"
+    },
+    {
+        name: "Project Management Site",
+        technologies: ["React JS", "Firebase", "Bootstrap"],
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam officiis culpa, eius pariatur repellat blanditiis mollitia cupiditate eligendi magni quasi natus suscipit sequi maxime laborum qui explicabo iste a!"
+    }
+]
+
+skills = [
+    {
+        title: "HTML 5",
+        symbol_url: "https://logodownload.org/wp-content/uploads/2016/10/html5-logo-11.png"
+    },
+    {
+        title: "CSS 3",
+        symbol_url: "https://cdn.iconscout.com/icon/free/png-256/free-css-alt-3628710-3029935.png"
+    },
+    {
+        title: "JavaScript",
+        symbol_url: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+        title: "Boostrap",
+        symbol_url: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/bootstrap-5-logo-icon.png"
+    },
+    {
+        title: "React JS",
+        symbol_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+    },
+    {
+        title: "Firebase",
+        symbol_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8zSZSg3SKMxv4_vk5Q6dVg5vrv5JXahIG_ghMcEtTfw&s"
+    },
+    {
+        title: "PHP",
+        symbol_url: "https://seeklogo.com/images/P/php-logo-91EFDB12E1-seeklogo.com.png"
+    },
+    {
+        title: "Express JS",
+        symbol_url: "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png"
+    },
+    
+]
